0316c3193c947c9a464fba77260814f8eb252375
Ever Barillas <everalf1995@gmail.com>
2018-03-14 03:58:02 -0400

    merged version between Ever and Khalid, 03/14/2018 3:57 AM

--- app/src/main/java/com/example/everb/kronicle/MainActivity.java
+++ app/src/main/java/com/example/everb/kronicle/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.everb.kronicle;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tablayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x;
-        boolean xx;
-    }
 
-    int y;
+        /* Create the tabLayout with fragments */
+        tablayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+
+        // Build Adapter object, and ADD ALL FRAGMENTS HERE
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new FragmentNotes(),"Notes");
+        adapter.AddFragment(new FragmentTimer(),"Timer");
+        adapter.AddFragment(new FragmentHabits(),"Habits");
+
+        // Adapter Setup
+        viewPager.setAdapter(adapter);
+        tablayout.setupWithViewPager(viewPager);
+
+    }
 }
