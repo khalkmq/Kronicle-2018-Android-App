93482aecc7d8f405bc321823b84edcd948adfca6
Khalid Q <khalkmq@gmail.com>
2018-03-15 21:07:34 -0400

    NEW ADDITIONS: - Added icons - Added New Strings - Created a Navigation sidemenu (Drawer) - Menu behaviour: Close menu after click - Menu behaviour: highlights tab user is on - Code modifications/comments to mainactivity java and xml
    STILL MISSING:
    - Hamburger Icon
    - Code to re-route menu items to new UI/page/view
    
    BUG MADE:
    - you can no longer click a tab to go to it. you must manually swipe to get there

--- app/src/main/java/com/example/everb/kronicle/MainActivity.java
+++ app/src/main/java/com/example/everb/kronicle/MainActivity.java
@@ -1,21 +1,45 @@
 package com.example.everb.kronicle;
 
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private TabLayout tablayout;
     private AppBarLayout appBarLayout;
     private ViewPager viewPager;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Drawer-SideMenu Setup */
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // Set item to Highlight
+                        menuItem.setChecked(true);
+                        // Close Side menu once clicked
+                        mDrawerLayout.closeDrawers();
+
+                        // We should add code here to redirect the UI to the page
+
+                        return true;
+                    }
+                });
+
+
 
         /* Create the tabLayout with fragments */
         tablayout = (TabLayout) findViewById(R.id.tablayout_id);
