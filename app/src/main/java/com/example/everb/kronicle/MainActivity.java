02ad30eabd7b408a55f07b9eb9efd130e4fd4bc1
Ever Barillas <everalf1995@gmail.com>
2018-03-16 16:19:18 -0400

    fixxed minor issues, cleaned up code

--- app/src/main/java/com/example/everb/kronicle/MainActivity.java
+++ app/src/main/java/com/example/everb/kronicle/MainActivity.java
@@ -23,38 +23,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Create the toolbar for the Hamburger!! */
+        // Create the toolbar for the Hamburger
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
-        /* Drawer-SideMenu Setup */
+        // Drawer-SideMenu Setup
         mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // Set item to Highlight
-                        menuItem.setChecked(true);
-                        // Close Side menu once clicked
-                        mDrawerLayout.closeDrawers();
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    // Set item to Highlight
+                    menuItem.setChecked(true);
+                    // Close Side menu once clicked
+                    mDrawerLayout.closeDrawers();
 
-                        // We should add code here to redirect the UI to the page
+                    // We should add code here to redirect the UI to the page
+                    return true;
+                }
+            });
 
-                        return true;
-                    }
-                });
+        // Create the tabLayout with fragments
+        tablayout = findViewById(R.id.tablayout_id);
+        viewPager = findViewById(R.id.viewpager_id);
 
-
-
-        /* Create the tabLayout with fragments */
-        tablayout = (TabLayout) findViewById(R.id.tablayout_id);
-        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
-
-        // Build Adapter object, and ADD ALL FRAGMENTS HERE
+        // Build Adapter object, Fragments go here
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.AddFragment(new FragmentNotes(),"Notes");
         adapter.AddFragment(new FragmentTimer(),"Timer");
@@ -65,13 +62,13 @@ public class MainActivity extends AppCompatActivity {
         tablayout.setupWithViewPager(viewPager);
     }
 
-    /* Menu icon Click behaviour; Opens SideMenu/drawer  */
+    // Menu icon Click behaviour; Opens SideMenu/drawer
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
