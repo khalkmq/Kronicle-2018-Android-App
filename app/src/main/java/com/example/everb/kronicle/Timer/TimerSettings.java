98076c7e59d1b8c4283e5b60768e58c36e20a9bf
Ever Barillas <everalf1995@gmail.com>
2018-04-20 17:43:09 -0400

    added "add" icon started working on "add new timer" page added new strings new activity for timer settings

---
+++ app/src/main/java/com/example/everb/kronicle/Timer/TimerSettings.java
@@ -0,0 +1,15 @@
+package com.example.everb.kronicle.Timer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.everb.kronicle.R;
+
+
+public class TimerSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timer_settings);
+    }
+}
