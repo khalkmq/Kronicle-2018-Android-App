5e2601a6d989e9fe37f802c4a6c0cc53abf17287
Ever Barillas <everalf1995@gmail.com>
2018-05-01 15:15:33 -0400

    - Notification

--- app/src/main/java/com/example/everb/kronicle/Settings.java
+++ app/src/main/java/com/example/everb/kronicle/Settings.java
@@ -1,18 +1,18 @@
 package com.example.everb.kronicle;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Settings extends AppCompatPreferenceActivity {
 
@@ -21,9 +21,7 @@ public class Settings extends AppCompatPreferenceActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
-
     }
 
     public static class MainPreferenceFragment extends PreferenceFragment {
@@ -32,9 +30,6 @@ public class Settings extends AppCompatPreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_main);
 
-
-
-            // feedback preference click listener
             Preference myPref = findPreference(getString(R.string.key_send_feedback));
             myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference preference) {
@@ -42,8 +37,39 @@ public class Settings extends AppCompatPreferenceActivity {
                     return true;
                 }
             });
+
+            Preference notificationSwitch = findPreference(getString(R.string.notifications_new_message));
+            notificationSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object boolChoice) {
+                    notifications(getActivity(), boolChoice);
+                    return true;
+                }
+            });
+
         }
     }
+
+    public static void notifications(Context context, Object boolChoice) {
+
+        boolean choice = (boolean) boolChoice;
+
+        if (choice) {
+            NotificationManager notification=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification notify = new Notification.Builder
+                    (context).setContentTitle("NOTIFICATION").setContentText("THIS WORKS IDK").
+                    setContentTitle("KRONICLE").setSmallIcon(R.drawable.k_simple).build();
+
+            notify.flags |= Notification.FLAG_AUTO_CANCEL;
+            notification.notify(0, notify);
+        }
+
+        else {
+            Toast.makeText(context, "You will stop receiving notifications", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -60,7 +86,9 @@ public class Settings extends AppCompatPreferenceActivity {
             body = "\nPlease don't remove the following information\n Device OS: Android \n Device OS version: " +
                     Build.VERSION.RELEASE + "\n App Version: " + body + "\n Device Brand: " + Build.BRAND +
                     "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER;
-        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        catch (PackageManager.NameNotFoundException e) {
         }
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
